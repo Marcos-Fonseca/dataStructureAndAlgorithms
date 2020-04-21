63ac0c22eee0bae13259556ce43337a42372c4ed
Marcos Fonseca <marcos.fonseca.armazenamento@gmail.com>
2020-04-21 00:15:43 -0300

    função melhorada pra trabalhar somente com um vetor

--- sort/SelectionSort/selectionsort.py
+++ sort/SelectionSort/selectionsort.py
@@ -1,24 +1,23 @@
 import random
 
 def SelectionSort(vector):
-    sortedVector = []
+    index = 0
 
-    while len(vector) > 0:
-        min = vector[0]
+    while index < len(vector):
+        minimum = vector[index]
 
-        for x in range(1, len(vector)):
-            if min > vector[x]:
-                min = vector[x]
+        for x in range(index+1, len(vector)):
+            if minimum > vector[x]:
+                minimum = vector[x]
 
-        sortedVector.append(min)
-        del(vector[vector.index(min)]) # delete selected element from original vector
-
-    return sortedVector
+        index2 = vector.index(minimum)
+        vector[index], vector[index2] = vector[index2], vector[index]
+        index += 1
 
 if __name__ == "__main__":
     vector = [x for x in range(100)]
     random.shuffle(vector)
 
     print(f'original vector = {vector}')
-    vector = SelectionSort(vector)
+    SelectionSort(vector)
     print(f'sorted vector = {vector}')

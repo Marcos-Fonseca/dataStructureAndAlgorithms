55f1b6b0ad0b21e9790accf3f0ee4820d7059d5d
Marcos Fonseca <marcos.fonseca.armazenamento@gmail.com>
2020-04-23 18:50:22 -0300

    insertion sort implementado

---
+++ sort/insertionSort/insertionSort.py
@@ -0,0 +1,20 @@
+import random
+
+def insertionSort(vector):
+    for x in range(1, len(vector)):
+        element = vector[x]
+        index = x
+
+        while index > 0 and vector[index-1] > element:
+            vector[index], vector[index-1] = vector[index-1], vector[index]
+            index -= 1
+        else:
+            vector[index] = element
+
+if __name__ == "__main__":
+    vector = [x for x in range(100)]
+    random.shuffle(vector)
+
+    print(f'original vector = {vector}')
+    insertionSort(vector)
+    print(f'sorted vector = {vector}')
